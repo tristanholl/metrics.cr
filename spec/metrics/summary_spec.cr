302dda77ab004407bb05b946d4ad49cc8d1d9c2e
Armin Pasalic <armin.pasalic@solarisbank.de>
2019-05-31 05:22:38 +0200

    Add elementary summary

---
+++ spec/metrics/summary_spec.cr
@@ -0,0 +1,32 @@
+require "spec"
+require "../../src/metrics_cr/metrics/summary"
+
+describe MetricsCr::Metrics::Summary do
+  describe "#observe" do
+    it "observes by the value provided" do
+      summary = MetricsCr::Metrics::Summary.new(name: "test")
+      summary.observe(2).should be nil
+    end
+  end
+
+  describe "#count" do
+    it "is a getter for count" do
+      summary = MetricsCr::Metrics::Summary.new(name: "test")
+      summary.count.should eq 0
+    end
+  end
+
+  describe "#sum" do
+    it "is a getter for sum" do
+      summary = MetricsCr::Metrics::Summary.new(name: "test")
+      summary.sum.should eq 0
+    end
+  end
+
+  describe "#value" do
+    it "is a getter for value" do
+      summary = MetricsCr::Metrics::Summary.new(name: "test")
+      summary.value.should eq({0, 0})
+    end
+  end
+end
